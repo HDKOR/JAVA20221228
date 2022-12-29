29543573437f99c981fa6776562c5eb3f5de51a2
21512835 <21512835@ynu.kr>
2022-12-29 18:01:03 +0900

    test

---
+++ src/Ch01/C01HelloWorld.java
@@ -0,0 +1,15 @@
+package Ch01; //패키지명(폴더오ㅏ 유사)
+
+public class C01HelloWorld // 클래스 영역- 객체지향문법 적용단위
+{
+	// 메서드(방법, 기능): 작업수행을 하는 데 필요한 여러코드를 묶어서 실행하는데 사용되는 단위
+	//main method		: 최초 실행함수
+	//library method	: 미리만들어서 제공되는 함수
+	//사용자 정의 메서드		: 개발자에 의해 만들어지는 함수
+	public static void main(String[] args) //매서드 영역- 절차지향문법 적용단위
+	{
+		
+		System.out.println("HELLO WORLD");
+	}
+
+}
